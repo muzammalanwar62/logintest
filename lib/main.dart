2bbe486857040ac33bed29e8c68faaff1ee06abd
muzammalanwar62 <30317849+muzammalanwar62@users.noreply.github.com>
2020-02-01 11:29:03 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(new login_page());
+}
+class login_page extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen
+      ),
+      home:Scaffold(
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Padding(padding: const EdgeInsets.only(top: 50.0)),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16.0,right: 16.0),
+              child: TextField(decoration: InputDecoration(labelText: "Username"),),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16.0,right: 16.0),
+              child: TextField(decoration: InputDecoration(labelText: "Password"),),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 16.0,right: 16.0,top: 16.0),
+              child:Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget>[
+                 Expanded(
+                  child: Container(
+                margin: const EdgeInsets.all(16.0),
+    ),
+              ),
+    ],
+    )
+            )
+          ],
+
+        ),
+      ),
+    );
+  }
+
+}
+
